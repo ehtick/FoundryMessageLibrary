6d85dd6998cff59137b731238cbb36e955a700c6
Steve <stephen.r.strong@saic.com>
2022-03-24 11:27:50 -0400

    adding cam

--- Math/Extensions.cs
+++ Math/Extensions.cs
@@ -2,7 +2,7 @@ namespace IoBTMessage.Models;
 
 using GeoJSON.Net.Feature;
 using GeoJSON.Net.Geometry;
-using IoBTMessage.Models;
+
 
 
 
@@ -44,6 +44,8 @@ static public class GeoMath
             return 0;
         }
     }
+
+
     static public IPosition asCoord(this Feature<Point> point)
     {
         return point.Geometry.Coordinates;
@@ -125,5 +127,45 @@ static public class GeoMath
         list.Add(pt2);
 
         return list;
+    }
+
+    static public Location computeGeoLocation(this HighResPosition pos, Location source)
+    {
+        var dist = Math.Sqrt(pos.xLoc * pos.xLoc + pos.zLoc * pos.zLoc);
+        var brg = Math.Atan2(pos.xLoc, pos.zLoc);
+        var feature = source.destination(dist, source.toDeg(brg));
+        var loc = Turf.GetCoord(feature.Geometry);
+
+        var result = new Location()
+        {
+            lat = loc[1],
+            lng = loc[0],
+            alt = source.alt + pos.yLoc,
+        };
+
+        return result;
+    }
+
+    static public HighResPosition computeHiresLocation(this Location source, Location target, string units = "kilometers")
+    {
+        var dist = source.distance(target);
+        var brg = source.bearing(target);
+        var rad = brg * Math.PI / 180;
+        var result = new HighResPosition()
+        {
+            units = units,
+            xLoc = dist * Math.Sin(rad),
+            yLoc = target.alt - source.alt,
+            zLoc = dist * Math.Cos(rad),
+            yAng = brg
+        };
+
+        return result;
+    }
+
+    static public Location computeGeoLocation(this Location source, HighResPosition target)
+    {
+        var result = target.computeGeoLocation(source);
+        return result;
     }
 }
