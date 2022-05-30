e6fd5a611e6338036e4e9322b1b0311d9cd0ccc6
Steve <stephen.r.strong@saic.com>
2022-05-30 15:05:11 -0400

    now with hydration

--- Models/UDTO_Label.cs
+++ Models/UDTO_Label.cs
@@ -6,6 +6,7 @@ public class UDTO_Label : UDTO_Base
 
     public string uniqueGuid { get; set; }
     public string targetGuid { get; set; }
+    public string labelName { get; set; }
     public string type { get; set; }
     public string text { get; set; }
     public string platformName { get; set; }
@@ -21,7 +22,7 @@ public class UDTO_Label : UDTO_Base
     {
         // 23 Fields
         // base (0, 1, 2, 3) uniqueGuid (4) bodyName (5) bodyType (6) symbol (7) platformName (8) position (9, 10, 11, 12, 13, 14, 15) 
-        return $"{base.compress(d)}{d}{uniqueGuid}{d}{targetGuid}{d}{type}{d}{text}{d}{platformName}{d}{data}{d}{position?.compress(d)}";
+        return $"{base.compress(d)}{d}{uniqueGuid}{d}{targetGuid}{d}{labelName}{d}{type}{d}{text}{d}{platformName}{d}{data}{d}{position?.compress(d)}";
     }
 
     public override int decompress(string[] inputData)
--- Models/UDTO_Sensor.cs
+++ Models/UDTO_Sensor.cs
@@ -8,6 +8,7 @@ public class UDTO_Sensor : UDTO_Base
     public string active { get; set; }
     public string extra { get; set; }
     public string container { get; set; }
+    public string source { get; set; }
 
     public UDTO_Sensor():base()
     {
@@ -15,7 +16,7 @@ public class UDTO_Sensor : UDTO_Base
 
     public override string compress(char d = ',')
     {
-        return $"{base.compress(d)}{d}{this.name}{d}{this.type}{d}{this.active}{d}{this.extra}{d}{this.container}";
+        return $"{base.compress(d)}{d}{this.name}{d}{this.type}{d}{this.active}{d}{this.extra}{d}{this.container}{d}{this.source}";
     }
 
     public override int decompress(string[] data)
@@ -26,11 +27,12 @@ public class UDTO_Sensor : UDTO_Base
         this.active = data[counter++];
         this.extra = data[counter++];
         this.container = data[counter++];
+        this.source = data[counter++];
         return counter;
     }
 
     public override string getUniqueCode()
     {
-        return $"{this.udtoTopic}{this.sourceGuid}{this.panID}{this.name}{this.type}{this.container}";
+        return $"{this.udtoTopic}{this.sourceGuid}{this.panID}{this.name}{this.type}{this.container}{this.source}";
     }
 }
