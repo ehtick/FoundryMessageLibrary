951f862821a475cee98f4c5008584ff9503495fb
Steve <stephen.r.strong@saic.com>
2022-06-17 21:58:21 -0400

    ok

--- Models/UDTO_Platform.cs
+++ Models/UDTO_Platform.cs
@@ -1,37 +1,5 @@
 namespace IoBTMessage.Models;
 
-
-[System.Serializable]
-public class UDTO_3D : UDTO_Base
-{
-	public string platformName { get; set; }
-	public string uniqueGuid { get; set; }
-	public string type { get; set; }
-	public string name { get; set; }
-
-	public bool isDelete()
-	{
-		return this.type == "Command:DELETE" ? true : false;
-	}
-
-	public override string compress(char d = ',')
-	{
-		return $"{base.compress(d)}{d}{platformName}{d}{uniqueGuid}{d}{type}{d}{name}{d}";
-	}
-
-	public override int decompress(string[] inputData)
-	{
-		var counter = base.decompress(inputData);
-
-		platformName = inputData[counter++];
-		uniqueGuid = inputData[counter++];
-		type = inputData[counter++];
-		name = inputData[counter++];
-		return counter;
-	}
-
-}
-
 [System.Serializable]
 public class UDTO_Platform : UDTO_3D
 {
