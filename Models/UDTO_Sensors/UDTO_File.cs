366af9834bdcda640c7a9df27905e17701c64487
Greg Fulton <gtfullton@gmail.com>
2023-02-10 13:49:31 -0500

    add width height to file

--- Models/UDTO_Sensors/UDTO_File.cs
+++ Models/UDTO_Sensors/UDTO_File.cs
@@ -7,6 +7,10 @@ namespace IoBTMessage.Models
 	{
 		public string url { get; set; }
 		public string filename { get; set; }
+		public int width { get; set; }
+		public int height { get; set; }
+		public string mimeType { get; set; }
+
 
 		//https://stackoverflow.com/questions/60797390/generate-random-image-by-url
 		public static SPEC_File RandomSpec()
@@ -14,9 +18,16 @@ namespace IoBTMessage.Models
 			var gen = new MockDataGenerator();
 			var width = 50 * gen.GenerateInt(0, 11);
 			var height = 50 * gen.GenerateInt(0, 11);
+
+			var filename = "default-name";
+			var mimeType = "image/png";
 			return new SPEC_File()
 			{
-				url = $"https://picsum.photos/{width}/{height}"
+				url = $"https://picsum.photos/{width}/{height}",
+				width = width,
+				height = height,
+				filename = filename,
+				mimeType = mimeType
 			};
 		}
 	}
@@ -25,6 +36,9 @@ namespace IoBTMessage.Models
 	{
 		public string url;
 		public string filename;
+		public int width;
+		public int height;
+		public string mimeType;
 
 		public UDTO_File() : base()
 		{
