0dce819279700102caa45ddd3bd9a8baa464b8f0
Steve <stephen.r.strong@saic.com>
2023-07-19 20:39:55 -0400

    ok

--- Models/DTAR/DT_Target.cs
+++ Models/DTAR/DT_Target.cs
@@ -5,6 +5,16 @@ using IoBTMessage.Extensions;
 
 namespace IoBTMessage.Models
 {
+	public class DT_Thread : DT_Searchable
+	{
+		public string sheetname { get; set; }
+		public string thread { get; set; }
+		public string resDes { get; set; }
+		public string controlNumber { get; set; }
+		public string serialNumber { get; set; }
+		public string revision { get; set; }
+	}
+
 
 	public class DO_Target : DO_Searchable
 	{
@@ -18,6 +28,7 @@ namespace IoBTMessage.Models
 	public class DT_Target : DT_Searchable
 	{
 		public DT_Part part;
+		public List<DT_Thread> threads;
 		public List<DT_AssetFile> models;
 		public List<DT_AssetFile> images;
 		public List<DT_Component> components;
@@ -33,6 +44,14 @@ namespace IoBTMessage.Models
 			return this.part;
 		}
 
+		public List<DT_Thread> AddThread(DT_Thread thread)
+		{
+			threads ??= new List<DT_Thread>();
+			if (thread != null)
+				threads.Add(thread);
+
+			return threads;
+		}
 
 		public List<DT_AssetFile> AddImage(DT_AssetFile image)
 		{
