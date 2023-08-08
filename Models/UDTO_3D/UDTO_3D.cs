433f138534f9dcc23dbf81df24901c9b804ec8bb
Steve <stephen.r.strong@saic.com>
2023-08-07 21:43:24 -0400

    ok

--- Models/UDTO_3D/UDTO_3D.cs
+++ Models/UDTO_3D/UDTO_3D.cs
@@ -25,7 +25,7 @@ namespace IoBTMessage.Models
 		public string uniqueGuid;
 		public string parentUniqueGuid;
 		public string referenceDesignation;
-		public DT_System system;
+		public DT_System subSystem;
 		public ControlParameters metadata;
 
 		public UDTO_3D(): base()
