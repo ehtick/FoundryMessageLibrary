ee4f33dd68889fc5d1594853ce8107d49909e90d
Steve <stephen.r.strong@saic.com>
2023-09-25 10:19:56 -0400

    ok

--- Models/DTAR/DT_Sensor.cs
+++ Models/DTAR/DT_Sensor.cs
@@ -5,11 +5,14 @@ namespace IoBTMessage.Models
 
 	public class DO_Sensor : DO_Component
 	{
+		public string topic { get; set; }
+		public string sourceGuid { get; set; }
 	}
 
 	public class DT_Sensor : DT_Component
 	{
-
+		public string topic;
+		public string sourceGuid;
 		public DT_Sensor() : base()
 		{
 		}
