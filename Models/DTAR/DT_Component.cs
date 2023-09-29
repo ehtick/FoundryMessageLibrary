42574e289eea1b8e6208c95ab58bcc4a95fd8248
Steve <stephen.r.strong@saic.com>
2023-09-28 20:19:56 -0400

    ok

--- Models/DTAR/DT_Component.cs
+++ Models/DTAR/DT_Component.cs
@@ -1,4 +1,6 @@
-﻿using System.Xml.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace IoBTMessage.Models
 {
@@ -11,10 +13,27 @@ namespace IoBTMessage.Models
 	public class DT_Component : DT_AssemblyItem
 	{
 
+		public List<DT_Component> members;
+
 		public DT_Component() : base()
 		{
 		}
 
+
+		public override List<DT_Hero> Children()
+		{
+			if (members == null) return base.Children();
+			return members.Select(item => (DT_Hero)item).ToList();
+		}
+
+		public DT_Component AddMember(DT_Component child)
+		{
+			members ??= new List<DT_Component>();
+			child.parentGuid = this.guid;
+			members.Add(child);
+			return child;
+		}
+		
 		public override DT_Component ShallowCopy()
 		{
 			var result = (DT_Component)this.MemberwiseClone();
@@ -22,6 +41,12 @@ namespace IoBTMessage.Models
 				result.part = (DT_Part)part.ShallowCopy();
 			result.heroImage = this.heroImage;
 
+			return result;
+		}
+
+		public List<DT_Component> ShallowMembers()
+		{
+			var result = members?.Select(obj => obj.ShallowCopy()).ToList();
 			return result;
 		}
 
