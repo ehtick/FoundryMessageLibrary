af1acbcb6c029111517c6418370cb07d1b94bbbc
Steve <stephen.r.strong@saic.com>
2023-11-16 05:11:40 -0500

    added size

--- Models/UDTO_3D/UDTO_3D.cs
+++ Models/UDTO_3D/UDTO_3D.cs
@@ -27,7 +27,7 @@ namespace IoBTMessage.Models
 		public string parentUniqueGuid;
 		public string referenceDesignation;
 		public DT_System subSystem;
-		//public ControlParameters metadata;
+
 
 		public UDTO_3D(): base()
 		{
