04cf6b2a3f5ae31b578a2c1621e9a16b46535ac9
Steve <stephen.r.strong@saic.com>
2024-04-18 08:46:48 -0400

    ok

---
+++ Models/DTAR/DT_Signal.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace IoBTMessage.Models
+{
+	public enum SignalElementType
+	{
+		Equipment,
+		Port,
+		Link,
+		Harness
+	}
+
+	public class DO_Signal : DT_Component
+	{
+		public string signal { get; set; }
+		public SignalElementType signalElementType { get; set; }
+
+		public string StartPortReference { get; set; }
+		public string FinishPortReference { get; set; }
+	}
+
+	[System.Serializable]
+	public class DT_Signal : DT_Component, ISystem
+	{
+		public string signal;
+		public SignalElementType signalElementType;
+
+		public string StartPortReference;
+		public string FinishPortReference;
+
+		public DT_Signal()
+		{
+		}
+
+
+		public override DT_Signal ShallowCopy()
+		{
+			var result = (DT_Signal)this.MemberwiseClone();
+
+			return result;
+		}
+
+	}
+}
+
+
