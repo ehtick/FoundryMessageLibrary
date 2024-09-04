a1a8950bfd725baa750d899add58ab7345fc3271
Steve <stephen.r.strong@saic.com>
2024-09-04 18:14:57 -0400

    Remove unused code files

--- Models/Modules/Spec_ComponentReference.cs
+++
@@ -1,9 +0,0 @@
-// https://beautifytools.com/excel-to-json-converter.php
-namespace IoBTMessage.Models;
-
-public class Spec_ComponentReference
-{
-
-    public string parentGuid { get; set; }
-    public string componentGuid { get; set; }
-}
